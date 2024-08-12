77b7730dda8aaaf1b8ed612eef46283d6f02b4a7
Hari <harisankarmahapatro@gmail.com>
2024-08-12 13:33:07 +0530

    Fix build issues

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',git add .
-git commit -m "Fix build issues"
-git push origin main
-
+  base: '/',
 })
